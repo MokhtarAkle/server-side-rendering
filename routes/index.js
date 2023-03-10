38561bc95eeb645f209264df3ac01b23bbd94420
MokhtarAkle <mokhtar.akle@hva.nl>
2023-03-10 07:24:00 +0100

    first implementation

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+const url = "https://api.codingthecurbs.fdnd.nl/api/v1/smartzones";
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  let smartUrl = url 
+
+  console.log(smartUrl)
+
+  fetchJson(url).then((data) => {
+    res.render('index', data)
+  })
+});
+
+
+
+async function fetchJson(url) {
+  return await fetch(url)
+    .then((response) => response.json())
+    .catch((error) => error)
+}
+
+module.exports = router;
