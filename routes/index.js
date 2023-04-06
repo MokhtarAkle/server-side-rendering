7380777b62af35e8c619bd3f84507d260feb21f0
MokhtarAkle <mokhtar.akle@hva.nl>
2023-04-06 09:46:26 +0200

    changes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const url = "https://api.codingthecurbs.fdnd.nl/api/v1/smartzones";
+const baseurl = "https://api.codingthecurbs.fdnd.nl/api/v1"
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -13,22 +14,44 @@ router.get('/', function(req, res, next) {
 });
 
 
+// router.post('/', (request, response) => {
+  
+
+  
+
+//   postJson(url1, request.body).then((data) => {
+//     let newZone = { ... request.body }
+//     console.log(data)
+//     if (data.success) {
+//       response.redirect('/?memberPosted=true') 
+//       // TODO: squad meegeven, message meegeven
+//       // TODO: Toast meegeven aan de homepagina
+
+//     } else {
+//       const errormessage = `${data.message}: Mogelijk komt dit door de slug die al bestaat.`
+//       const newdata = { error: errormessage, values: newZone }
+
+//       }
+//   })
+// })
+
 router.post('/', (request, response) => {
-  const baseurl = "https://api.codingthecurbs.fdnd.nl/api/v1"
-
+  request.body.timeStart = request.body.dateStart + 'T' + request.body.timeStart + ':00Z';
+  request.body.timeEnd = request.body.dateEnd + 'T' + request.body.timeEnd + ':00Z';    
   const url1 = `${baseurl}/reservations`
-
   postJson(url1, request.body).then((data) => {
-    let newZone = { ... request.body }
-    console.log(newZone)
-    if (data.success) {
-      response.redirect('/?memberPosted=true') 
+    let newReservation = { ... request.body}
+    console.log(JSON.stringify(data))
 
+   if (data.success) {
+        response.redirect('/?reservationPosted')
+        console.log("yep")
 
-    } else {
-      const errormessage = `${data.message}: Mogelijk komt dit door de slug die al bestaat.`
-      const newdata = { error: errormessage, values: newZone }
-
+    }
+    else {
+    const errorMessage = data.message
+    const newData = { error: errorMessage, values: newReservation }
+    console.log(newData)
     }
   })
 })
